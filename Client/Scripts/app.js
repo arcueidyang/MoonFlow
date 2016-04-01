c3b184d17a754825e5050a8ad29511394b687225
Yang Yang <arcueidyang@gmail.com>
2016-03-31 23:38:49 -0700

    made more try, seems not very compatible, decide to switch to another approach

--- Client/Scripts/app.js
+++ Client/Scripts/app.js
@@ -2,18 +2,21 @@
  * Created by Yang on 3/17/16.
  */
 
-var moonflow = angular.module("moonflow", ["ngRoute"])
-    .config(function($routeProvider) {
-        $routeProvider.when("/newPost", {
-            templateUrl: "Views/NewPost.html",
-            controller: "NewPostController"
+define(["angular", "require", "angular-route]"], function (angular, require) {
+    var moonflow = angular.module("moonflow", ["ngRoute"])
+        .config(function($routeProvider) {
+            $routeProvider.when("/newPost", {
+                templateUrl: "Views/NewPost.html",
+                controller: "NewPostController"
+            });
+            $routeProvider.when("/about", {
+                templateUrl: "Views/About.html",
+                controller: "AboutController"
+            });
+            $routeProvider.when("/blog", {
+                templateUrl: "Views/Blog.html",
+                controller: "BlogController"
+            });
         });
-        $routeProvider.when("/about", {
-            templateUrl: "Views/About.html",
-            controller: "AboutController"
-        });
-        $routeProvider.when("/blog", {
-            templateUrl: "Views/Blog.html",
-            controller: "BlogController"
-        });
-    });
+
+});
