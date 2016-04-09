33dcafa237ca641409e4681810b7f9a4a4a90abd
Yang Yang <arcueidyang@gmail.com>
2016-04-08 23:47:24 -0700

    still making gundam today

--- Client/Scripts/main.ts
+++ Client/Scripts/main.ts
@@ -1,7 +1,6 @@
 /// <reference path="../Library/require.d.ts" />
 /// <reference path="../Library/angular.d.ts" />
 
-
 //https://arnhem.luminis.eu/using-typescript-angular-require-and-existing-javascript-libraries-the-great-altogether/
 
 require.config({
