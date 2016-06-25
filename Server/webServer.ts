34eb4c964b3e260fe5058cb9a60d3cff6339d125
Yang Yang <arcueidyang@gmail.com>
2016-06-24 23:48:53 -0700

    converted root server to typescript

--- Server/webServer.ts
+++ Server/webServer.ts
@@ -1,19 +1,28 @@
 /// <reference path="../typings/body-parser/body-parser.d.ts" />
 /// <reference path="../typings/express/express.d.ts" />
 /// <reference path="../typings/mongoose/mongoose.d.ts" />
-"use strict";
-var body_parser_1 = require("body-parser");
-var express = require("express");
-var mongoose = require("mongoose");
+
+
+import { json, urlencoded } from "body-parser";
+import * as express from "express";
+import * as mongoose from "mongoose";
 var path = require("path");
+
 var db = mongoose.connect("mongodb://localhost/bookAPI");
+
 var app = express();
+
 var port = 3000;
-app.use(express.static(path.join(__dirname, "../")));
-app.use(body_parser_1.urlencoded({ extended: true }));
-app.use(body_parser_1.json());
+
+app.use(express.static(path.join(__dirname, "../")))
+
+app.use(urlencoded({ extended: true }));
+app.use(json());
+
 var router = require("./Routes/BaseRouter")();
+
 app.use("/api", router);
-app.listen(port, function () {
+
+app.listen(port, function() {
     console.log("server has been set up and listening to port: " + port);
 });
