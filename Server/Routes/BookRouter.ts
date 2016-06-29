07b44c428647d120295d2a59b2cc7d994b1c7821
Yang Yang <arcueidyang@gmail.com>
2016-06-28 23:36:43 -0700

    also convert book router to typescript

--- Server/Routes/BookRouter.ts
+++ Server/Routes/BookRouter.ts
@@ -3,82 +3,83 @@
 import * as express from "express";
 import BookTest = require("../Models/BookModel");
 
-var routes = function() {
-    var router = express.Router();
-    router.route("/")
-        .post(function(req, res) {
-            var book = new BookTest(req.body);
-            book.save();
-            res.status(201).send(book);
-        })
-        .get(function(req, res) {
-            var query = req.query;
-            BookTest.find(query, function(err, books){
+export class BookRouter {
+
+    public GetBookRouter() {
+        var router = express.Router();
+        router.route("/")
+            .post(function(req, res) {
+                var book = new BookTest(req.body);
+                book.save();
+                res.status(201).send(book);
+            })
+            .get(function(req, res) {
+                var query = req.query;
+                BookTest.find(query, function(err, books){
+                    if(err) {
+                        res.status(500).send(err);
+                    } else {
+                        res.json(books);
+                    }
+                });
+            });
+
+        router.use("/:bookId", (req, res, next) => {
+            BookTest.findById(req.params.bookId, function(err, book) {
                 if(err) {
                     res.status(500).send(err);
+                } else if(book) {
+                    req.book = book;
+                    next();
                 } else {
-                    res.json(books);
+                    res.status(404).send("Can't find the book");
                 }
             });
-        });
+        });    
 
-    router.use("/:bookId", (req, res, next) => {
-        BookTest.findById(req.params.bookId, function(err, book) {
-            if(err) {
-                res.status(500).send(err);
-            } else if(book) {
-                req.book = book;
-                next();
-            } else {
-                res.status(404).send("Can't find the book");
-            }
-        });
-    });    
-
-    router.route("/:bookId")
-        .get(function(req, res) {
-            res.json(req.book);
-        })
-        .put(function(req, res){
-        	UpdateItem(req.book, req.body);
-            req.book.save();
-            req.json(req.book);            
-        })
-        .patch(function(req, res) {
-            if(req.body._id) {
-                delete req.body._id;
-            }
-            for(var property in req.body) {
-                if(req.body.hasOwnProperty(property)) {
-                    req.book[property] = req.body[property];
-                }
-            }
-
-            req.book.save(function(err) {
-                if(err) {
-                    res.status(500).send(err);    
-                } else {
-                    res.json(req.book);
-                }
+        router.route("/:bookId")
+            .get(function(req, res) {
+                res.json(req.book);
             })
-        })
-        .delete(function(req, res) {
-            req.book.remove(function(err) {
-                if(err) {
-                    res.status(500).send(err);    
-                } else {
-                    res.status(204).send("Removed");
+            .put(function(req, res){
+            	this.UpdateItem(req.book, req.body);
+                req.book.save();
+                req.json(req.book);            
+            })
+            .patch(function(req, res) {
+                if(req.body._id) {
+                    delete req.body._id;
                 }
+                for(var property in req.body) {
+                    if(req.body.hasOwnProperty(property)) {
+                        req.book[property] = req.body[property];
+                    }
+                }
+
+                req.book.save(function(err) {
+                    if(err) {
+                        res.status(500).send(err);    
+                    } else {
+                        res.json(req.book);
+                    }
+                });
+            })
+            .delete(function(req, res) {
+                req.book.remove(function(err) {
+                    if(err) {
+                        res.status(500).send(err);    
+                    } else {
+                        res.status(204).send("Removed");
+                    }
+                });
             });
-        });
 
-    return router;
-};
+        return router;
+    }
 
-function UpdateItem(itemToUpdate, sourceItem) {
-    itemToUpdate.title = sourceItem.title;
-    itemToUpdate.author = sourceItem.author;
-    itemToUpdate.genre = sourceItem.genre;
+    public UpdateItem(itemToUpdate, sourceItem) {
+        itemToUpdate.title = sourceItem.title;
+        itemToUpdate.author = sourceItem.author;
+        itemToUpdate.genre = sourceItem.genre;
+    }
 }
-
-module.exports = routes;
