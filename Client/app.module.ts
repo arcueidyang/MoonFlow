8ed6309e72c4dc01eec6ff442e7423c6b6082273
Yang Yang <arcueidyang@gmail.com>
2016-09-26 21:44:49 -0700

    fixed http issue

--- Client/app.module.ts
+++ Client/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
+import { HttpModule, JsonpModule } from '@angular/http';
 import { FormsModule }    from '@angular/forms';
 import { AppComponent }         from './app.component';
 import { routing, appRoutingProviders }  from './app.routing';
@@ -13,6 +14,8 @@ import { BookComponent } from "./App/Book/Book";
     imports: [
         BrowserModule,
         FormsModule,
+        HttpModule,
+        JsonpModule,
         routing
     ],
     declarations: [
